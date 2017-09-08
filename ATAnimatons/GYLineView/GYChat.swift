b9ba9f4c8c9608caf8591265e9dcf2cc6fd0dde3
airfight <745756619@qq.com>
2017-09-08 09:12:45 +0800

    折线图

---
+++ ATAnimatons/GYLineView/GYChat.swift
@@ -0,0 +1,28 @@
+//
+//  GYChat.swift
+//  ATAnimatons
+//
+//  Created by ZGY on 2017/9/8.
+//  Copyright © 2017年 macpro. All rights reserved.
+//
+//  Author:        Airfight
+//  My GitHub:     https://github.com/airfight
+//  My Blog:       http://airfight.github.io/
+//  My Jane book:  http://www.jianshu.com/users/17d6a01e3361
+//  Current Time:  2017/9/8  上午9:12
+//  GiantForJade:  Efforts to do my best
+//  Real developers ship.
+
+import UIKit
+
+class GYChat: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
